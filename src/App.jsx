649814042a51f9b4c9f7c2888b3414a066ce9c4e
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2025-07-31 23:52:14 +0530

    Tailwind or daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from './Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='/about' element={<div>About page</div>}/>
+        <Route path='/profile' element={<div>Profile page</div>}/>
+        <Route path='/login' element={<div>Login page</div>}/>
+        <Route path='/register' element={<div>Register page</div>}/>
+      </Routes>
+    </BrowserRouter>
+    <Navbar/>
+    </>
   )
 }
 
