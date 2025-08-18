2ba48b47f746aa553b6f6e4d0dbbe1f8855782d0
Nayan Kar <167174966+NayanKarIITP@users.noreply.github.com>
2025-08-18 23:47:35 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-export const BASE_URL = "/api";
-
-//export const BASE_URL = "http://localhost:5000";
+export const BASE_URL =
+    location.hostname === "localhost" ?"http://localhost:5000" : "/api";
