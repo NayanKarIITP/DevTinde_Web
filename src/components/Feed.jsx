5d0f0849884939bf053b528b6f80858f0774bd9e
Nayan Kar <167174966+NayanKarIITP@users.noreply.github.com>
2025-12-15 00:38:45 +0530

    Refactor Feed component for clarity and efficiency
    Refactor Feed component to improve readability and remove unnecessary imports. Simplified API call and error handling.

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,22 +1,67 @@
-import React, { useEffect } from 'react'
-import api from "../utils/api";
-import { useSelector, useDispatch } from 'react-redux'
-import { BASE_URL } from '../utils/constants'
-import { addFeed } from '../utils/feedSlice';
-import Usercard from './Usercard'
-import api from "../utils/api";
+// import React, { useEffect } from 'react'
+// import api from "../utils/api";
+// import { useSelector, useDispatch } from 'react-redux'
+// import { BASE_URL } from '../utils/constants'
+// import { addFeed } from '../utils/feedSlice';
+// import Usercard from './Usercard'
+// import api from "../utils/api";
+
+// const Feed = () => {
+//   const feed = useSelector((store) => store.feed);
+//   const dispatch = useDispatch();
+
+//   const getFeed = async() => {
+//     if (feed) return;
+//     try {
+//       const res= await api.get(BASE_URL+'/feed',{withCredentials:true});
+//       dispatch(addFeed(res.data));
+//     } catch (error) {
+//       //TODO: Handle error
+//       console.error(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getFeed();
+//   }, []);
+
+//   if(!feed) return;
+
+//   if(feed.length <=0) return <h1 className='text-center text-white text-2xl mt-7'>There are no users</h1>;
+//   return (
+//     feed?.length > 0  &&(
+//     <div className='flex justify-center my-10'>
+//       <Usercard user={feed[0]}/>
+//     </div>
+//     )
+//   )
+// }
+
+// export default Feed;
+
+
+
+
+
+
+
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import Usercard from "./Usercard";
+import api from "../utils/api"; // ✅ ONLY ONE api import
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
 
-  const getFeed = async() => {
+  const getFeed = async () => {
     if (feed) return;
+
     try {
-      const res= await api.get(BASE_URL+'/feed',{withCredentials:true});
+      const res = await api.get("/feed"); // ✅ NO BASE_URL
       dispatch(addFeed(res.data));
     } catch (error) {
-      //TODO: Handle error
       console.error(error);
     }
   };
@@ -25,16 +70,21 @@ const Feed = () => {
     getFeed();
   }, []);
 
-  if(!feed) return;
+  if (!feed) return null;
 
-  if(feed.length <=0) return <h1 className='text-center text-white text-2xl mt-7'>There are no users</h1>;
+  if (feed.length <= 0) {
+    return (
+      <h1 className="text-center text-white text-2xl mt-7">
+        There are no users
+      </h1>
+    );
+  }
+
   return (
-    feed?.length > 0  &&(
-    <div className='flex justify-center my-10'>
-      <Usercard user={feed[0]}/>
+    <div className="flex justify-center my-10">
+      <Usercard user={feed[0]} />
     </div>
-    )
-  )
-}
+  );
+};
 
 export default Feed;
