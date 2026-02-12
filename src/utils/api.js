582e80695317644126849daad7670982b20b15ce
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2026-02-12 13:02:21 +0530

    restore axios api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,15 @@
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_BACKEND_URL,
+//   withCredentials: true,
+// });
+
+// export default api;
+
+
+
 import axios from "axios";
 
 const api = axios.create({
