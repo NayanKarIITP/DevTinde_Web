1bcd89b1db1cb69d904cc65b103eda804e72db7a
Nayan Kar <167174966+nkar90@users.noreply.github.com>
2026-02-20 12:21:24 +0530

    updated socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,8 +1,28 @@
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
+// import { BASE_URL } from './constants';
+
+// export const  createSocketConnection = () => {
+//     if(location.hostname === "localhost") return io(BASE_URL);
+//     else return io("/",{path:"/api/socket.io"});
+// };
+// export default createSocketConnection;
+
+
+import { io } from 'socket.io-client';
 import { BASE_URL } from './constants';
 
-export const  createSocketConnection = () => {
-    if(location.hostname === "localhost") return io(BASE_URL);
-    else return io("/",{path:"/api/socket.io"});
+export const createSocketConnection = () => {
+    if (location.hostname === "localhost") {
+        // Local development
+        return io(BASE_URL, {
+            withCredentials: true
+        });
+    } else {
+        // Production: Point directly to Render, NOT "/"
+        return io("https://devtinder-c1y5.onrender.com", {
+            withCredentials: true
+        });
+    }
 };
+
 export default createSocketConnection;
